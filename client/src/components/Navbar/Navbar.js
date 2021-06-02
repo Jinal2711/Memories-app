04291b600a8fb733a88ab52875bb8ba351ba7e7e
Vaghasiya <jinal.vaghasiya@here.com>
2021-06-02 20:40:54 +0530

    fix: eslint errors

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
     }
     // TODO: JWT signup for manula signup process
     setUser(JSON.parse(localStorage.getItem("profile")));
-  }, [location, user?.token, logout]);
+  }, [location, user?.token]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
